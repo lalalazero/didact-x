8b775047035411571dee11e0f137909e3558d4ce
lalalazero <zzzero520@hotmail.com>
2021-02-26 18:09:03 +0800

    first update: render

--- my-react.js
+++ my-react.js
@@ -1,2 +1,45 @@
-function render(){
+function render(element, container, callback){
+    return legacyRenderSubtreeIntoContainer(
+        null,
+        element,
+        container,
+        false,
+        callback
+    )
+}
+
+function legacyRenderSubtreeIntoContainer(parentComponent, children, container, forceHydrate, callback){
+    let root = container._reactRootContainer
+    let fiberRoot;
+    if(!root) {
+        // Initial mount 初次渲染
+        root = container._reactRootContainer = legacyCreateRootFromDOMContainer(container, forceHydrate)
+        fiberRoot = root._internalRoot;
+
+        if(typeof callback === 'function') {
+            const originalCallback = callback 
+            const callback = function(){
+                const instance = getPublicRootInstance(fiberRoot)
+                originalCallback.call(instance)
+            }
+        }
+        // Initial mount should not be batched.
+        unbatchedUpdates(()=>{
+            updateContainer(children, fiberRoot, parentComponent, callback)
+        })
+    }else{
+        fiberRoot = root._internalRoot
+        if(typeof callback === 'function') {
+            const originalCallback = callback 
+            const callback = function(){
+                const instance = getPublicRootInstance(fiberRoot)
+                originalCallback.call(instance)
+            }
+        }
+
+        // Update
+        updateContainer(children, fiberRoot, parentComponent, callback)
+    }
+
+    return getPublicRootInstance(fiberRoot)
 }
